4a2ee2f54828704c79147421e07ffa0ff9c9df61
Jamie Hamilton (P110103851) <P110103851@student.peterborough.ac.uk>
2019-09-27 15:55:26 +0100

    Constructor method added

--- Program.cs
+++ Program.cs
@@ -9,4 +9,26 @@ namespace basic_shapes
             Console.WriteLine("Hello World!");
         }
     }
+
+    class Rectangle
+    {
+        int height;
+        int width;
+
+        static void createRectangles()
+        {
+            Rectangle rectangle1 = new Rectangle();
+            Rectangle rectangle2 = new Rectangle();
+            Rectangle rectangle3 = new Rectangle();
+
+            rectangle1.height = 25;
+            rectangle1.width = 34;
+
+            rectangle2.height = 5;
+            rectangle2.width = 10;
+
+            rectangle3.height = 30;
+            rectangle3.width = 15;
+        }
+    }
 }
